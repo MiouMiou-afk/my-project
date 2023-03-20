c3937936b08720c82d37a02b731d46f8ea95cb51
Myriam <myriam@cinetwork.app>
2023-03-20 17:56:07 +0100

    first commit

---
+++ src/app/app-module.ts
@@ -0,0 +1,23 @@
+import {  NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { HeaderComponent } from './components/header/header-component';
+import { MainComponent } from './components/main/main-component';
+import { LogoComponent } from './components/logo/logo-component';
+import { NavComponent } from './components/nav/nav-component';
+import { AppComponent } from "./app-component";
+
+@NgModule({
+    declarations: [
+      MainComponent,
+      AppComponent,
+      HeaderComponent,
+      NavComponent,
+      LogoComponent,
+    ],
+    imports: [BrowserModule],
+    bootstrap: [AppComponent],
+  })
+ export class AppModule {}
+  
+  // platformBrowserDynamic().bootstrapModule(RootModule);
